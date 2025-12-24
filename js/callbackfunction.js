fa2553d03bf5b322393c6099a94a945c2635876e
Shahnawaz-Bhutto <hajibooshanu777@gmail.com>
2025-12-24 21:52:29 +0500

    add a js file

---
+++ js/callbackfunction.js
@@ -0,0 +1,12 @@
+setTimeout(function(a,b) {
+    console.log(2+3);
+}, 10000);
+setTimeout(function() {
+    console.log("adding a and b");
+}, 8000);
+setTimeout(function(a,b) {
+    console.log(2-3);
+}, 6000);
+setTimeout(function(a,b) {
+    console.log("Substracting a and b");
+}, 3000);
